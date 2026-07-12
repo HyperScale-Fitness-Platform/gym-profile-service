682536d8b9369a93a234f8b0378dc63d907ada36
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-07-12 03:58:45 +0300

    added service routes

---
+++ src/routes/profile.routes.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const profileController = require("../controllers/profile.controller");
+const validateRequest = require("../middleware/validateRequest.middleware");
+
+const router = express.Router();
+
+router.post(
+  "/customers",
+  validateRequest(["user_id", "full_name"]),
+  profileController.createCustomer,
+);
+router.get("/customers", profileController.listCustomerProfiles);
+router.get("/customers/:id", profileController.getCustomer);
+router.get("/customers/by-user/:user_id", profileController.getCustomer);
+router.put("/customers/:id", profileController.updateCustomerProfile);
+router.delete("/customers/:id", profileController.removeCustomer);
+
+router.post(
+  "/trainers",
+  validateRequest(["user_id", "full_name"]),
+  profileController.createTrainer,
+);
+router.get("/trainers", profileController.listTrainerProfiles);
+router.get("/trainers/:id", profileController.getTrainer);
+router.get("/trainers/by-user/:user_id", profileController.getTrainer);
+router.put("/trainers/:id", profileController.updateTrainerProfile);
+router.delete("/trainers/:id", profileController.removeTrainer);
+
+router.post(
+  "/certifications",
+  validateRequest(["trainer_id", "title"]),
+  profileController.createCertificationEntry,
+);
+router.get(
+  "/trainers/:trainerId/certifications",
+  profileController.listCertifications,
+);
+router.get("/certifications/:id", profileController.getCertification);
+router.put("/certifications/:id", profileController.updateCertificationEntry);
+router.delete("/certifications/:id", profileController.removeCertification);
+
+module.exports = router;
