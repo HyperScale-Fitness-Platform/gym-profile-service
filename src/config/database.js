4489ff52161411b46158fff0fc3fe7f0cb95b2f7
Ibrahim-Elsayed-27 <himahema858@gmail.com>
2026-07-15 07:02:53 +0300

    make connecting to db acceptable by two ways according to env passed

--- src/config/database.js
+++ src/config/database.js
@@ -2,14 +2,26 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DB,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl:
-    process.env.PGSSLMODE === "require" ? { rejectUnauthorized: false } : false,
-});
+const pool = new Pool(
+  process.env.DATABASE_URL
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl:
+          process.env.PGSSLMODE === "require"
+            ? { rejectUnauthorized: false }
+            : false,
+      }
+    : {
+        host: process.env.POSTGRES_HOST,
+        port: Number(process.env.POSTGRES_PORT),
+        database: process.env.POSTGRES_DB,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        ssl:
+          process.env.PGSSLMODE === "require"
+            ? { rejectUnauthorized: false }
+            : false,
+      },
+);
 
 module.exports = { pool };
